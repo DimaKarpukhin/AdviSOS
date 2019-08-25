ea49b46f25554c44d2ad4b73c81a066ff64b662a
Ronen <liro3103@gmail.com>
2019-08-25 22:41:14 +0300

    Added 'Rate' method to 'Rating'

--- app/src/main/java/com/studymobile/advisos/Models/Rating.java
+++ app/src/main/java/com/studymobile/advisos/Models/Rating.java
@@ -3,7 +3,7 @@ package com.studymobile.advisos.Models;
 public class Rating
 {
     private long mVotersNum;
-    private float mAvgRating;
+    private float mAvgRating; /* between 1 to 5 */
 
     public Rating(){}
     public Rating(long i_VotersNum, float i_AvgRating)
@@ -30,5 +30,16 @@ public class Rating
     public void SetAvgRating(float i_AvgRating)
     {
         this.mAvgRating = i_AvgRating;
+    }
+
+    public void Rate(int i_Score)
+    {
+        if(i_Score < 1 || i_Score > 5)
+        {
+            throw new IllegalArgumentException (String.format("A user cannot be rated with a score of %d", i_Score));
+        }
+
+        mVotersNum++;
+        mAvgRating += 1 / mVotersNum * i_Score;
     }
 }
