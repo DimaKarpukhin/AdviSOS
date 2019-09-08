50648956a988431c6645f05e3b60f1a1e6aa2a0a
Shota Sepiashvilli <shotas@imperva.com>
2019-09-08 15:47:04 +0300

    minor push changes

--- app/src/main/java/com/studymobile/advisos/Activities/ActivityUserDetails.java
+++ app/src/main/java/com/studymobile/advisos/Activities/ActivityUserDetails.java
@@ -106,49 +106,49 @@ public class ActivityUserDetails extends AppCompatActivity implements View.OnCli
         setContentView(R.layout.activity_user_details);
         setFirebaseData();
         setActivityContent();
-        if(isUserExistsInDatabase())
-        {
-            getUserPersonalDetailsFromDB();
-        }
+//        if(isUserExistsInDatabase())
+//        {
+//           // getUserPersonalDetailsFromDB();
+//        }
     }
 
-    private boolean isUserExistsInDatabase()
-    {
-        //TODO check if this implementation suffice.
-        DatabaseReference ref = m_Database.getReference("Users");
-        ref.child(m_CurrentUser.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(dataSnapshot.exists())
-                    m_IsUserExists = true;
-                else
-                    m_IsUserExists = false;
-            }
+//    private boolean isUserExistsInDatabase()
+//    {
+//        //TODO check if this implementation suffice.
+//        DatabaseReference ref = m_Database.getReference("Users");
+//        ref.child(m_CurrentUser.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                if(dataSnapshot.exists())
+//                    m_IsUserExists = true;
+//                else
+//                    m_IsUserExists = false;
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+//        return m_IsUserExists;
+//    }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-        return m_IsUserExists;
-    }
-
-    private void getUserPersonalDetailsFromDB()
-    {
-        //TODO dont know how much data we have at this point, but location is must at this point
-       DatabaseReference reference =m_Database.getReference("Users").child(m_CurrentUser.getUid());
-       reference.addListenerForSingleValueEvent(new ValueEventListener() {
-           @Override
-           public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-               m_UserLocation =(dataSnapshot.child("Location").getValue(UserLocation.class));
-           }
-
-           @Override
-           public void onCancelled(@NonNull DatabaseError databaseError) {
-
-           }
-       });
-    }
+//    private void getUserPersonalDetailsFromDB()
+//    {
+//        //TODO dont know how much data we have at this point, but location is must at this point
+//       DatabaseReference reference =m_Database.getReference("Users").child(m_CurrentUser.getUid());
+//       reference.addListenerForSingleValueEvent(new ValueEventListener() {
+//           @Override
+//           public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//               m_UserLocation =(dataSnapshot.child("Location").getValue(UserLocation.class));
+//           }
+//
+//           @Override
+//           public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//           }
+//       });
+//    }
 
     @Override
     protected void onStart()
